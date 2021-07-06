339cc29828dfc0281868ed13b449813adb5c9351
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-06 20:53:32 +0300

    1

---
+++ Следующий раунд/Следующий раунд/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Следующий_раунд
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            int n = Convert.ToInt32(input[0]);
+            int k = Convert.ToInt32(input[1]);
+
+            string[] scores = Console.ReadLine().Split(" ");
+            Console.WriteLine(scores.Where(x => Convert.ToInt32(x) >= Convert.ToInt32(scores[k - 1]) && Convert.ToInt32(x) > 0).Count());
+        }
+    }    
+}
