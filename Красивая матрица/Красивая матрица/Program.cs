73f025b56efc8ff1acba0194f817d9b91fa3fe55
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-06 20:48:01 +0300

    1

--- Красивая матрица/Красивая матрица/Program.cs
+++ Красивая матрица/Красивая матрица/Program.cs
@@ -6,7 +6,17 @@ namespace Красивая_матрица
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[][] s = new string[5][];
+            for (int i = 0; i < 5; i++)
+                s[i] = Console.ReadLine().Split(' ');
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (s[i][j] == "1")
+                        Console.WriteLine(Math.Abs(3 - (i + 1)) + Math.Abs(3 - (j + 1)));
+                }
+            }
         }
     }
 }
