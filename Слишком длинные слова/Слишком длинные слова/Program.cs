2d1c4c8c77d4aaa8703b65b600418744d68e6c58
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-06 20:55:14 +0300

    1

---
+++ Слишком длинные слова/Слишком длинные слова/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Слишком_длинные_слова
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string word = Console.ReadLine();
+
+                if (word.Length > 10)
+                    word = $"{word[0]}{word.Length - 2}{word[word.Length - 1]}";
+
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
