328ef618882b85cc7aaa0fa0faac5931da840af8
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-06 20:54:25 +0300

    1

---
+++ Команда/Команда/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Команда
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < N; i++)
+            {
+                result += Console.ReadLine().Split(" ").Where(x => x == "1").Count() > 1 ? 1 : 0;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
