578ffaf3ea7f4f4d0d9ac7aaefafced661a74f95
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-06 20:47:17 +0300

    1

---
+++ Красивая матрица/Красивая матрица/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Красивая_матрица
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
