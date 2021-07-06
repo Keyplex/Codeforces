312463a590a773751e69430ffc51593e65beb709
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-06 20:49:35 +0300

    1

---
+++ Петя и строки/Петя и строки/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Петя_и_строки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine().ToLower();
+            string b = Console.ReadLine().ToLower();
+            Console.WriteLine(a.CompareTo(b));
+        }
+    }
+}
