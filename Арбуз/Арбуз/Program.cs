29574d6c13c424ce09741b7ffa22fef4a57deb99
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-06 20:56:29 +0300

    1

---
+++ Арбуз/Арбуз/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Арбуз
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var w = int.Parse(Console.ReadLine());
+
+            var isGreat2AndDivideBy2 = w > 2 && w % 2 == 0;
+
+            var an = isGreat2AndDivideBy2 ? "YES" : "NO";
+
+            Console.WriteLine(an);
+        }
+    }
+}
