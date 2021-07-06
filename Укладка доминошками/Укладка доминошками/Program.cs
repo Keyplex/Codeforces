2f784681644fc77b394d2232cc81e9c4bda8d588
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-06 20:52:31 +0300

    1

---
+++ Укладка доминошками/Укладка доминошками/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Укладка_доминошками
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] serch = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Console.WriteLine(serch[0] * (serch[1] / 2) + (serch[1] % 2) * (serch[0] / 2));
+            Console.ReadLine();
+        }
+    }
+}
