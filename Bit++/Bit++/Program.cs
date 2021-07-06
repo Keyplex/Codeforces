49b41dc42adb6a5d2a94ba80e04236d64e71fa5a
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-06 20:50:31 +0300

    1

---
+++ Bit++/Bit++/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int r = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                if (s == "X++" || s == "++X")
+                {
+                    r++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+            Console.WriteLine(r);
+        }
+    }
+}
