41a3ee5c9221dfb41be311f5598cf669c2735e60
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-20 17:29:01 +0300

    1

---
+++ Математика спешит на помощь/Математика спешит на помощь/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Математика_спешит_на_помощь
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            var number = s.Split("+").OrderBy(x => Convert.ToInt32(x));
+            var result = string.Empty;
+            foreach (var item in number)
+            {
+                result += item + "+";
+            }
+            Console.WriteLine(result.Trim('+'));
+        }
+    }
+}
