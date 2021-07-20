99621c17f48467037a6fa30565e3681e1709311f
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-20 17:40:30 +0300

    1

---
+++ Камни на столе/Камни на столе/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Камни_на_столе
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            char[] c = s.ToCharArray();
+            int count = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (c[i-1]==c[i])                
+                    count++;            
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Капитализация слова/Капитализация слова/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Капитализация_слова
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(char.ToUpper(a[0]) + a.Substring(1));
+        }
+    }
+}
