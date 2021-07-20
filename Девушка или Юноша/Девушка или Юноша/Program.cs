cd6e613f10186c414e65fee8f1120bf45fc5efba
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-20 17:45:25 +0300

    1

---
+++ Девушка или Юноша/Девушка или Юноша/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Девушка_или_Юноша
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Console.ReadLine().Distinct().Count();
+            if ((n&1)==1)
+            {
+                Console.WriteLine("IGNORE HIM!");
+            }
+            else
+            {
+                Console.WriteLine("CHAT WITH HER!");
+            }               
+        }
+    }
+}
