653423f1575e2cc2d84d4ad21be8e2dbd37c5204
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-20 17:50:52 +0300

    1

---
+++ Солдат и бананы/Солдат и бананы/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Солдат_и_бананы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int k = int.Parse(s[0]);
+            int n = int.Parse(s[1]);
+            int w = int.Parse(s[2]);
+            int count = 0;
+            for (int i = 1; i <= w; i++)
+            {
+                count += k * i;
+            }
+            int d = (count > n) ? (count - n) : 0;
+            Console.WriteLine(d);
+        }
+    }
+}
