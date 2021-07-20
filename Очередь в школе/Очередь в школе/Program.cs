160c3ab2f39c6f220d11947cb577dae1aa00b9bf
Pavel <51826654+1commando1@users.noreply.github.com>
2021-07-20 18:03:14 +0300

    1

---
+++ Очередь в школе/Очередь в школе/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Очередь_в_школе
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Console.ReadLine().Split(' ');
+            int n = int.Parse(id[0]);
+            int x = int.Parse(id[1]);
+            var str = new StringBuilder(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < n-1; j++)
+                {
+                    if (str[i] == 'B' && str[i+1] == 'G')
+                    {
+                        str[i] = 'G';
+                        str[i + 1] = 'B';
+                        i++;
+                    }
+                }
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
